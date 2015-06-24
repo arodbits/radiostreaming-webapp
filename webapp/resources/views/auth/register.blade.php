38e113047a3735f7443e2bb38e90c203b9e21d34
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-06-24 18:54:30 -0400

    Remove bootstrap panel

--- webapp/resources/views/auth/register.blade.php
+++ webapp/resources/views/auth/register.blade.php
@@ -1,11 +1,8 @@
-@extends('app')
+@extends('layout.plain')
 @section('content')
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
