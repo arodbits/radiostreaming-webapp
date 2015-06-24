1d998388988cc51157318851e4743370eb6d3217
=Anthony Rodriguez <AnthonyRodriguez.itt@gmail.com>
2015-06-24 14:36:56 -0400

    Pass user argument to UserWasRegister class

--- webapp/app/Events/UserWasRegistered.php
+++ webapp/app/Events/UserWasRegistered.php
@@ -7,15 +7,15 @@ use Illuminate\Queue\SerializesModels;
 class UserWasRegistered extends Event {
 
 	use SerializesModels;
-
+	protected $user;
 	/**
 	 * Create a new event instance.
 	 *
 	 * @return void
 	 */
-	public function __construct()
+	public function __construct($user)
 	{
-		//
+		$this->user = $user;
 	}
 
 }
